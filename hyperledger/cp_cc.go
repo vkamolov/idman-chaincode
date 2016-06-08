9e780860764136925f4b37a52ce529d5d307edcd
Viktor Kamolov <vkamolov@gmail.com>
2016-06-09 00:53:33 +1000

    company verifications

--- hyperledger/cp_cc.go
+++ hyperledger/cp_cc.go
@@ -697,11 +697,11 @@ func VerifyCompany(stub *shim.ChaincodeStub, sCompany string) (Company, error){
 		return company, errors.New("Company " + company.ID + " not found")
 	}
 
-	//Verifications
-	if ( company.RegDate  != companyDB.RegDate  ||
-	     company.RegState != companyDB.RegState ||
-	     (company.ACN && company.ACN != companyDB.ACN) ||
-	     (company.ABN && company.ABN != companyDB.ABN) ) {
+	//Verifications (we don't check name. cause it's a part of the key)
+	if 	(company.RegDate != companyDB.RegDate) || 
+		(company.RegState != companyDB.RegState) ||
+		((company.ACN != "") && (company.ACN != companyDB.ACN)) ||
+		((company.ABN != "") && (company.ABN != companyDB.ABN)) {
 
 		return company, errors.New("Company verification failed")
 	}
